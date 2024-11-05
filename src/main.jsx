ed807e672d39af958fd0019c3911e5d3f72dfce2
Falodent <falodave11@gmail.com>
2024-11-05 19:37:00 +0100

    Message

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-const Main = () => {
-  return (
-    <div>Main Page</div>
-  )
-}
-export default Main
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
